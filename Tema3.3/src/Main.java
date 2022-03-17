93e2e792c8b57ac23e928e515b5df370990aabb9
Sami00002 <94164764+Sami00002@users.noreply.github.com>
2022-03-17 20:03:29 +0200

    Add files via upload

---
+++ Tema3.3/src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println(Suma.Suma(1, 2 ,3, 4));
+
+	}
+//la exercitiul aceasta si la 3, am cerut un pic de ajutor, n-am copiat, am inteles. 
+}
---
+++ Tema3.3/src/Suma.java
@@ -0,0 +1,15 @@
+
+public class Suma {
+	
+public static int Suma(int a, int b){
+	return a+b;
+}
+
+public static int Suma(int a, int b, int c) {
+	return Suma(a,b) + c;
+}
+public static int Suma(int a, int b, int c,int d) {
+	return Suma(a,b,c) + d;
+}
+}
+	
